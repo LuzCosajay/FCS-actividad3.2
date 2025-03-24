b4c74f3a67fc99fee8e9d6774ccf0731e52ca282
LuzCosajay <luz.cosajay@galileo.edu>
2025-03-23 21:13:51 -0600

    Add files via upload
    Hola mundo

---
+++ hola_mundo.php
@@ -0,0 +1,8 @@
+<?php
+// Mostrar el mensaje en pantalla
+echo "<h1>Hola mundo, soy Luz Cosajay</h1>";
+?>
+
+<!-- Mostrar la imagen que moviste -->
+<img src="Flux_Dev_A_woman_in_her_30s_with_short_stylishly_messy_brown_h_2.jpeg" style="width:300px;height:auto;">
+
