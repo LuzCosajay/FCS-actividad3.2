d3a4be354d805d6ff6d6bfea7db68192e00c3e9a
LuzCosajay <luz.cosajay@galileo.edu>
2025-03-25 04:11:42 +0000

    Delete hola_mundo.php

--- hola_mundo.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Mostrar el mensaje en pantalla
-echo "<h1>Hola mundo, soy Luz Cosajay</h1>";
-?>
-
-<!-- Mostrar la imagen que moviste -->
-<img src="Flux_Dev_A_woman_in_her_30s_with_short_stylishly_messy_brown_h_2.jpeg" style="width:300px;height:auto;">
-
